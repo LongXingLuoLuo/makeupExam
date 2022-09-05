effb9b9fea20a7c8a1ba71acaf5b7b7a24507c6c
Lxll <2473475362@qq.com>
2022-09-05 22:23:53 +0800

    添加项目文件。

---
+++ 补考/main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+class Student {
+public:
+	Student(const char* name) {
+		strcpy_s(this->name, name);
+	}
+	char* getName() {
+		return this->name;
+	}
+private:
+	char name[50];
+};
+
+int two() 
+{
+	return 0;
+}
---
+++ 补考/输入输出.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+void showGetline() {
+	char str[50];
+	cout << "输入:\n";
+	cin.getline(str, 50);
+	cout << "输出：\n";
+	cout << str;
+}
+void test() {
+	char str[100] = { 0 };
+	cin.getline(str, 100);
+
+	char c1 = 0, c2 = 0;
+	char str1[100] = { 0 };
+	int p = 0;
+	for (int i = 0; i < 100; i++) {
+		if ((i == 0 || str[i] != str[i - 1]) && (i == 99 || str[i] != str[i + 1])) {
+			str1[p] = str[i];
+			p++;
+		}
+		if (str[i] == '\0') {
+			break;
+		}
+	}
+	cout << str1;
+}
+int main() {
+	char source[100] = {"one"};
+	char target[100];
+	// 如果strcpy有提醒，那么推荐用strcpy_s
+	// strcpy_s(target, source);
+	strcpy(target, source);
+	cout << "source is " << source <<endl;
+	cout << "after strcpy " << endl;
+	cout << "target is " << target<<endl;
+	return 0;
+}
